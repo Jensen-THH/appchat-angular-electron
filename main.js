918ddfad2448686ea6ba00ff27534ea213aff591
hiep <65339132+hiep523@users.noreply.github.com>
2021-07-16 13:21:05 +0700

    signin

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function createWindow() {
 // win.webContents.setUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0";
 // win.webContents.setUserAgent(win.webContents.getUserAgent() + " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
 // win.webContents.setUserAgent(win.webContents.getUserAgent() + "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
-// const menu = Menu.buildFromTemplate([]);
-// Menu.setApplicationMenu(menu);
+const menu = Menu.buildFromTemplate([]);
+Menu.setApplicationMenu(menu);
     win.loadURL(`http://localhost:4200`,{userAgent: 'Edge'});
 
 }
